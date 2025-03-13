f52acb496a0a9c6e742c9b4822b0bc8b16841800
Olavi Allik <olaviallik@gmail.com>
2025-03-13 17:28:43 +0200

    Update head.php

--- views/components/head.php
+++ views/components/head.php
@@ -7,3 +7,13 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/@unicorn-fail/drupal-bootstrap-styles@0.0.2/dist/3.4.0/8.x-3.x/drupal-bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" media="all" href="<?=$site_url;?>assets/css/style2.css" />
+<!-- Matomo Tag Manager -->
+<script>
+  var _mtm = window._mtm = window._mtm || [];
+  _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+  (function() {
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/takeda.matomo.cloud/container_icGpggiS.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Tag Manager -->
